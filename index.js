6fcd64acedf59d0ca5f9b3bde961b2761382174a
admacas <admacas@espol.edu.ec>
2021-11-17 21:40:09 -0500

    modiciando variables

--- index.js
+++ index.js
@@ -9,10 +9,11 @@ const app = express();
 conectarDB();
 
 // habilitar cors
-const opcionesCors = {
-    origin: process.env.FRONTEND_URL
-}
-app.use(cors(opcionesCors));
+// const opcionesCors = {
+//     origin: process.env.FRONTEND_URL
+// }
+// app.use(cors(opcionesCors));
+app.use(cors());
 
 console.log('Comenzando NodeSend');
 // puerto de la app
